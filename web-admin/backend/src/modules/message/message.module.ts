2255e213170bbfcb38a25303ecca91f5111ff84a
miyunguyen <ya15510@gmail.com>
2026-05-16 11:01:50 +0700

    feat(message): implement sendTeamsMessage functionality and related DTOs

--- web-admin/backend/src/modules/message/message.module.ts
+++ web-admin/backend/src/modules/message/message.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { MessageService } from './message.service';
 import { MessageController } from './message.controller';
 import { RocketChatModule } from '../rocketChat/rocketChat.module';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-  imports: [RocketChatModule],
+  imports: [RocketChatModule, PrismaModule],
   providers: [MessageService],
   controllers: [MessageController],
 })
